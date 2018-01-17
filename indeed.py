83357f801d35fe01ffd075b3bce45a94578fb726
hyyc116 <hyyc116@gmail.com>
2018-01-17 19:29:21 +0800

    add

--- indeed.py
+++ indeed.py
@@ -5,7 +5,7 @@ import os
 from utils import *
 from collections import defaultdict
 import re
-
+from bs4 import BeautifulSoup as BS
 
 
 ## mapping location to MSA
@@ -30,18 +30,27 @@ def date_count_statistic():
     for location in location_count.keys():
         print location,location_count[location]
 
-def salary():
+def parse():
     for f in os.listdir('../Pages'):
         content = open('../Pages/'+f).read().strip().replace("\\u002F",'/')
 
         if 'salary' in content or '$' in content:
-            print f,content
+            salary =  extract_salary(content)
+            print f+"=="+salary
+
+def extract_salary(content):
+    regex=re.compile("\$\d+,?\d*\.?\d*\s?\-?t?o?\s?\$?\d+,?\d*\.?\d*\s?\/\s?\w{2,5}")
+    ss = []
+    for s in regex.findall(content):
+        ss.append(s)
+    return ','.join(ss)
 
 
 
 if __name__ == '__main__':
     # date_count_statistic()
-    salary()
+    # salary()
+    parse()
 
 
 
